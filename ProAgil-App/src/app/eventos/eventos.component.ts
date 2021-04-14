5c45a373f3d311510939a77a414d62c1e2fd5056
Augusto França <augusto.franca@hotmail.com>
2021-04-14 10:48:57 -0300

    Adicionado Angular SPA

---
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.css']
+})
+export class EventosComponent implements OnInit {
+
+  eventos: any;
+
+  constructor(private http: HttpClient) { }
+
+  // tslint:disable-next-line: typedef
+  ngOnInit() {
+    this.getEventos();
+  }
+
+  // tslint:disable-next-line: typedef
+  getEventos(){
+    // tslint:disable-next-line: deprecation
+    this.http.get('http://localhost:5000/api/values').subscribe(response => {
+      this.eventos = response;
+    }, error => {
+      console.log(error);
+    }
+    );
+  }
+
+}
